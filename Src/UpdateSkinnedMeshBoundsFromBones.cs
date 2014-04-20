5a5156e9df209c6fa9db2fb9137e7bd82c2950fa
Justin Joel Ward <JustinJoelWard@gmail.com>
2014-04-19 19:52:58 -0500

    Update and rename MB2_UpdateSkinnedMeshBoundsFromBones.cs to UpdateSkinnedMeshBoundsFromBones.cs

--- Src/UpdateSkinnedMeshBoundsFromBones.cs
+++ Src/UpdateSkinnedMeshBoundsFromBones.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using System.Collections;
 using DigitalOpus.MB.Core;
 
-public class MB2_UpdateSkinnedMeshBoundsFromBones : MonoBehaviour {
+public class UpdateSkinnedMeshBoundsFromBones : MonoBehaviour {
     SkinnedMeshRenderer smr;
 	Transform[] bones;
      
 	void Start () {
 		smr = GetComponent<SkinnedMeshRenderer>();
 		if (smr == null){
-			Debug.LogError("Need to attach MB2_UpdateSkinnedMeshBoundsFromBones script to an object with a SkinnedMeshRenderer component attached.");
+			Debug.LogError("Need to attach UpdateSkinnedMeshBoundsFromBones script to an object with a SkinnedMeshRenderer component attached.");
 			return;
 		}
 		bones = smr.bones;
@@ -19,7 +19,7 @@ public class MB2_UpdateSkinnedMeshBoundsFromBones : MonoBehaviour {
     
 	void Update () {
         if (smr != null){
-			MB2_MeshCombiner.UpdateSkinnedMeshApproximateBoundsFromBonesStatic(bones,smr);
+			MeshCombiner.UpdateSkinnedMeshApproximateBoundsFromBonesStatic(bones,smr);
 		}
 	}
 }
