d9089bdd63adc78b2ffb77ca446f0b569e115a22
Justin Joel Ward <JustinJoelWard@gmail.com>
2014-04-19 20:04:02 -0500

    Changed to match new conventions

--- Src/TextureBakeResults.cs
+++ Src/TextureBakeResults.cs
@@ -8,33 +8,24 @@ using System.Collections.Generic;
 /// Used internally during the material baking process
 /// </summary>
 [Serializable]
-public class MB_AtlasesAndRects{
+public class AtlasesAndRects{
 	public Texture2D[] atlases;
 	public Dictionary<Material,Rect> mat2rect_map;
 	public string[] texPropertyNames;
 }
 
 [System.Serializable]
-public class MB_MultiMaterial{
+public class MultiMaterial{
 	public Material combinedMaterial;
 	public List<Material> sourceMaterials = new List<Material>();
 }
 
-/// <summary>
-/// This class stores the results from an MB2_TextureBaker when materials are combined into atlases. It stores
-/// a list of materials and the corresponding UV rectangles in the atlases. It also stores the configuration
-/// options that were used to generate the combined material.
-/// 
-/// It can be saved as an asset in the project so that textures can be baked in one scene and used in another.
-/// 
-/// </summary>
-
-public class MB2_TextureBakeResults : ScriptableObject {
-	public MB_AtlasesAndRects[] combinedMaterialInfo;
+public class TextureBakeResults : ScriptableObject {
+	public AtlasesAndRects[] combinedMaterialInfo;
 	public Material[] materials;
 	public Rect[] prefabUVRects;
 	public Material resultMaterial;
-	public MB_MultiMaterial[] resultMaterials;
+	public MultiMaterial[] resultMaterials;
 	public bool doMultiMaterial;
 	public bool fixOutOfBoundsUVs;
 	
