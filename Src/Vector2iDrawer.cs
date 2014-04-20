deba3e16e8225d0fa766c8f6c2a03fd398e2c070
dxcam1 <justinjoelward@gmail.com>
2014-04-19 20:31:56 -0500

    Added Property Drawer for Vector2i

---
+++ Src/Vector2iDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Survive{
+	[CustomPropertyDrawer(typeof(Vector2i))]
+	public class Vector2iDrawer : PropertyDrawer{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label){
+		EditorGUI.BeginProperty(position,label,property);
+		
+		position = EditorGUI.PrefixLabel (position, GUIUtility.GetControlID (FocusType.Passive), label);
+		
+		int indent = EditorGUI.indentLevel;
+		EditorGUI.indentLevel = 0;
+		
+		//Drawer positionRects
+		Rect xRect = new Rect (position.x-40, position.y, 100, position.height);
+		Rect yRect = new Rect (position.x+90, position.y, 100, position.height);
+		
+		//EditorFields
+		EditorGUI.PropertyField (xRect, property.FindPropertyRelative ("x"), GUIContent.none);
+		EditorGUI.PropertyField (yRect, property.FindPropertyRelative ("y"), GUIContent.none);
+		EditorGUI.indentLevel = indent;
+		
+		EditorGUI.LabelField(new Rect(position.x-60,position.y, 100, position.height),"x");
+		EditorGUI.LabelField(new Rect(position.x+70,position.y, 100, position.height),"y");
+		EditorGUI.EndProperty();
+		
+		}
+	}
+}
